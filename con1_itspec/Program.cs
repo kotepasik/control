158280443a5672fc5478563b453dceb8ef50476e
kotepasik <kotepasik@gmail.com>
2022-09-14 18:05:40 +0400

    метод2

--- con1_itspec/Program.cs
+++ con1_itspec/Program.cs
@@ -11,3 +11,16 @@ void PrintArray(string[] array)
     }
     Console.WriteLine();
 }
+
+void StringsLessThenThree(string[] array1, string[] array2)
+{
+    int index = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[index] = array1[i];
+            index++;
+        }
+    }
+}
